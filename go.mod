51d7f252fcbc09234fc22d452c8dab20500b1795
Prashant Baghel <prashantbaghel215@gmail.com>
2022-11-27 00:27:54 +0530

    feat: added cli support

--- go.mod
+++ go.mod
@@ -9,6 +9,10 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/urfave/cli/v2 v2.23.5 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/exp v0.0.0-20221114191408-850992195362 // indirect
 )
--- main.go
+++ main.go
@@ -8,7 +8,11 @@ import (
 
 	"github.com/go-redis/redis/v8"
 	"github.com/samber/lo"
+	"github.com/urfave/cli/v2"
 )
+
+var host string
+var port string
 
 func getMatchingKeys(pattern string, matchedKeys chan []string, client *redis.Client) {
 	var cursor uint64
@@ -33,39 +37,65 @@ func getMatchingKeys(pattern string, matchedKeys chan []string, client *redis.Cl
 }
 
 func main() {
-	argsWithoutProg := os.Args[1:]
-	lenghtOfArgsWithoutProg := len(argsWithoutProg)
-	if lenghtOfArgsWithoutProg == 0 {
-		log.Fatalln("No key patterns provided")
-	}
+	app := &cli.App{
+		Name:  "redisdel",
+		Usage: "Scan for redis keys matching a given pattern and delete them",
+		Version: "0.1.0",
+		Action: func(cCtx *cli.Context) error {
+			argsWithoutProg := cCtx.Args().Slice()
+			lenghtOfArgsWithoutProg := len(argsWithoutProg)
+			if lenghtOfArgsWithoutProg == 0 {
+				log.Fatalln("No key patterns provided")
+			}
 
-	client := newRedisClient("redisdel.conf")
-	if client == nil {
-		log.Fatalln("Failed to initialise redis client")
-	}
+			client := newRedisClient("redisdel.conf")
+			if client == nil {
+				log.Fatalln("Failed to initialise redis client")
+			}
 
-	matchedKeysChans := make([]chan []string, lenghtOfArgsWithoutProg)
-	for i := range matchedKeysChans {
-		matchedKeysChans[i] = make(chan []string)
-	}
+			matchedKeysChans := make([]chan []string, lenghtOfArgsWithoutProg)
+			for i := range matchedKeysChans {
+				matchedKeysChans[i] = make(chan []string)
+			}
 
-	for i, pattern := range argsWithoutProg {
-		go getMatchingKeys(pattern, matchedKeysChans[i], client)
-	}
+			for i, pattern := range argsWithoutProg {
+				go getMatchingKeys(pattern, matchedKeysChans[i], client)
+			}
 
-	for i := range matchedKeysChans {
-		keys := <-matchedKeysChans[i]
-		log.Printf("Found %s keys for pattern %d", argsWithoutProg[i], len(keys))
-		chunkedKeys := lo.Chunk(keys, 1000)
-		var wg sync.WaitGroup
-		for batch, chunk := range chunkedKeys {
-			wg.Add(1)
-			go func(chunk []string, batch int) {
-				defer wg.Done()
-				client.Del(context.Background(), chunk...)
-				log.Printf("Deleted batch %d of %d for %s", batch+1, len(chunkedKeys), argsWithoutProg[i])
-			}(chunk, batch)
-		}
-		wg.Wait()
+			for i := range matchedKeysChans {
+				keys := <-matchedKeysChans[i]
+				log.Printf("Found %s keys for pattern %d", argsWithoutProg[i], len(keys))
+				chunkedKeys := lo.Chunk(keys, 1000)
+				var wg sync.WaitGroup
+				for batch, chunk := range chunkedKeys {
+					wg.Add(1)
+					go func(chunk []string, batch int) {
+						defer wg.Done()
+						client.Del(context.Background(), chunk...)
+						log.Printf("Deleted batch %d of %d for %s", batch+1, len(chunkedKeys), argsWithoutProg[i])
+					}(chunk, batch)
+				}
+				wg.Wait()
+			}
+			return nil
+	},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+					Name:  "host",
+					Usage: "redis host",
+					Destination: &host,
+					Required: true,
+			},
+			&cli.StringFlag{
+				Name:  "port",
+				Usage: "redis port",
+				Destination: &port,
+				Required: true,
+		},
+	},
+}
+
+if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
 	}
 }
