08057bc4b1ed9aa7bf705a7b2e48514be7d2a047
Prashant Baghel <prashantbaghel215@gmail.com>
2022-12-02 17:54:06 +0530

    chore: cde update, take username, password from cli

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/samber/lo v1.35.0
+	github.com/urfave/cli/v2 v2.23.5
 )
 
 require (
@@ -12,7 +13,6 @@ require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/urfave/cli/v2 v2.23.5 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	golang.org/x/exp v0.0.0-20221114191408-850992195362 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -14,6 +15,79 @@ import (
 
 var host string
 var port string
+var username string
+var password string
+
+func getRedisConfig(cfgFile string) *RedisConfig {
+	data, err := os.ReadFile(cfgFile)
+	if err != nil {
+		log.Fatalln("Failed to read redis del config")
+	}
+	var config RedisConfig
+	json.Unmarshal(data, &config)
+	return &config
+}
+
+func appAction(cCtx *cli.Context) error {
+	argsWithoutProg := cCtx.Args().Slice()
+	lenghtOfArgsWithoutProg := len(argsWithoutProg)
+	if lenghtOfArgsWithoutProg == 0 {
+		log.Fatalln("No key patterns provided")
+	}
+
+	addr := fmt.Sprintf("%s:%s", host, port)
+	fileConfig := getRedisConfig("redisdel.conf")
+	redisServerConfig := RedisServerConfigs{
+		Address: addr,
+	}
+	if redisServerConfig.Address == "" {
+		redisServerConfig.Address = fileConfig.ServerConfigs.Address
+	}
+
+	redisConfig := RedisConfig{
+		Username: username,
+		Password: password,
+		ServerConfigs: &redisServerConfig,
+		SentinelConfigs: fileConfig.SentinelConfigs,
+	}
+	if redisConfig.Username == "" {
+		redisConfig.Username = fileConfig.Username
+	}
+	if redisConfig.Password == "" {
+		redisConfig.Password = fileConfig.Password
+	}
+
+	client := newRedisClient(redisConfig)
+	if client == nil {
+		log.Fatalln("Failed to initialise redis client")
+	}
+
+	matchedKeysChans := make([]chan []string, lenghtOfArgsWithoutProg)
+	for i := range matchedKeysChans {
+		matchedKeysChans[i] = make(chan []string)
+	}
+
+	for i, pattern := range argsWithoutProg {
+		go getMatchingKeys(pattern, matchedKeysChans[i], client)
+	}
+
+	for i := range matchedKeysChans {
+		keys := <-matchedKeysChans[i]
+		log.Printf("Found %d keys for pattern %s", len(keys), argsWithoutProg[i])
+		chunkedKeys := lo.Chunk(keys, 1000)
+		var wg sync.WaitGroup
+		for batch, chunk := range chunkedKeys {
+			wg.Add(1)
+			go func(chunk []string, batch int) {
+				defer wg.Done()
+				client.Del(context.Background(), chunk...)
+				log.Printf("Deleted batch %d of %d for %s", batch, len(chunkedKeys), argsWithoutProg[i])
+			}(chunk, batch)
+		}
+		wg.Wait()
+	}
+	return nil
+}
 
 func getMatchingKeys(pattern string, matchedKeys chan []string, client *redis.Client) {
 	var cursor uint64
@@ -42,45 +116,7 @@ func main() {
 		Name:  "redisdel",
 		Usage: "Scan for redis keys matching a given pattern and delete them",
 		Version: "0.1.0",
-		Action: func(cCtx *cli.Context) error {
-			argsWithoutProg := cCtx.Args().Slice()
-			lenghtOfArgsWithoutProg := len(argsWithoutProg)
-			if lenghtOfArgsWithoutProg == 0 {
-				log.Fatalln("No key patterns provided")
-			}
-
-			addr := fmt.Sprintf("%s:%s", host, port)
-			client := newRedisClient("redisdel.conf", addr)
-			if client == nil {
-				log.Fatalln("Failed to initialise redis client")
-			}
-
-			matchedKeysChans := make([]chan []string, lenghtOfArgsWithoutProg)
-			for i := range matchedKeysChans {
-				matchedKeysChans[i] = make(chan []string)
-			}
-
-			for i, pattern := range argsWithoutProg {
-				go getMatchingKeys(pattern, matchedKeysChans[i], client)
-			}
-
-			for i := range matchedKeysChans {
-				keys := <-matchedKeysChans[i]
-				log.Printf("Found %s keys for pattern %d", argsWithoutProg[i], len(keys))
-				chunkedKeys := lo.Chunk(keys, 1000)
-				var wg sync.WaitGroup
-				for batch, chunk := range chunkedKeys {
-					wg.Add(1)
-					go func(chunk []string, batch int) {
-						defer wg.Done()
-						client.Del(context.Background(), chunk...)
-						log.Printf("Deleted batch %d of %d for %s", batch+1, len(chunkedKeys), argsWithoutProg[i])
-					}(chunk, batch)
-				}
-				wg.Wait()
-			}
-			return nil
-	},
+		Action: appAction,
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 					Name:  "host",
@@ -93,11 +129,21 @@ func main() {
 				Usage: "redis port",
 				Destination: &port,
 				Required: true,
+			},
+			&cli.StringFlag{
+				Name:  "username",
+				Usage: "redis username",
+				Destination: &username,
+			},
+			&cli.StringFlag{
+				Name:  "password",
+				Usage: "redis password",
+				Destination: &password,
+			},
 		},
-	},
-}
+	}
 
-if err := app.Run(os.Args); err != nil {
+	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
 }
